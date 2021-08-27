10e2e66565f2cf9a7e6d0e58224f9b80cd2fabea
Sneglen99 <69890147+Sneglen99@users.noreply.github.com>
2021-08-27 12:48:45 +0200

    Add files via upload

---
+++ Photoresistor/Photoresistor.ino
@@ -0,0 +1,13 @@
+int photoPin = A0;
+
+void setup() {
+  //pinMode(photoPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int readValue = analogRead(photoPin); 
+  Serial.println("Analog Value :");
+  Serial.println(readValue);  
+  delay(500);
+}
