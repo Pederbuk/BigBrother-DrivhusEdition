ddc4575452a49b6c10e6268ed606ee310d5e47b9
bardpedersen <72338855+bardpedersen@users.noreply.github.com>
2021-08-27 13:15:35 +0200

    Add files via upload

---
+++ Jordsensor.ino
@@ -0,0 +1,27 @@
+const int AirValue = 609;   //Tallene sensoren får i luft
+const int WaterValue = 256;  //Tallene sensoren får i vann
+int soilMoistureValue = 0;
+int soilmoisturepercent=0;
+void setup() {
+  Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
+}
+void loop() {
+soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
+Serial.println(soilMoistureValue);
+soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+if(soilmoisturepercent >= 100)
+{
+  Serial.println("100 %");
+}
+else if(soilmoisturepercent <=0)
+{
+  Serial.println("0 %");
+}
+else if(soilmoisturepercent >0 && soilmoisturepercent < 100)
+{
+  Serial.print(soilmoisturepercent);
+  Serial.println("%");
+  
+}
+  delay(1000);
+}
