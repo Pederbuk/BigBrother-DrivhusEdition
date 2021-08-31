0c5552796461f2aa520d4551172123be046f7b69
bardpedersen <72338855+bardpedersen@users.noreply.github.com>
2021-08-31 09:27:09 +0200

    Update Jordsensor.ino

--- Capacitive Soil Moisture Sensor/Jordsensor.ino
+++ Capacitive Soil Moisture Sensor/Jordsensor.ino
@@ -9,19 +9,6 @@ void loop() {
 soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
 Serial.println(soilMoistureValue);
 soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-if(soilmoisturepercent >= 100)
-{
-  Serial.println("100 %");
-}
-else if(soilmoisturepercent <=0)
-{
-  Serial.println("0 %");
-}
-else if(soilmoisturepercent >0 && soilmoisturepercent < 100)
-{
-  Serial.print(soilmoisturepercent);
-  Serial.println("%");
-  
-}
-  delay(1000);
+Serial.print(soilmoisturepercent);
+Serial.println("%");
 }
