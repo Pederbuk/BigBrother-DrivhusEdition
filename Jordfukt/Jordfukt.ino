a8c4e0bf9ee61fc3c7122bc62fc07e29fed538cb
SigmundGrimsrud <89636813+SigmundGrimsrud@users.noreply.github.com>
2021-08-31 13:16:16 +0200

    Add files via upload

--- Jordfukt/Jordfukt.ino
+++ Jordfukt/Jordfukt.ino
@@ -1,13 +1,21 @@
-int thresholdUp = 400;
-int thresholdDown = 250;
+int thresholdUp = 550; //Tørt
+int thresholdDown = 300; //Vått
 
+//Veldig tørr jord ute = 557 og 540
 
 int sensorPin = A0;
 
+//Water pump
+int relePin = 2;
 
 void setup(){
   Serial.begin(9600);
+
+  pinMode(relePin, OUTPUT); 
 }
+
+int needWater = 0;
+int waitTime = 5;
 
 void loop(){
   
@@ -24,19 +32,41 @@ void loop(){
   Serial.println(WaterLevel);
   
 
-  if (sensorValue < thresholdDown){
+  if (sensorValue > thresholdUp){
     String DisplayWords = "Dry, Water it!";
+    needWater = needWater + 1;
+    if (needWater >= waitTime){
+      StartPump();
+      delay(2000);
+      StopPump();
+      needWater = 0;
+    }
     Serial.print(DisplayWords);
     
 
   
-  } else if (sensorValue > thresholdUp){
+  } else if (sensorValue < thresholdDown){
     String DisplayWords = "Wet, Leave it!";
-    Serial.print(DisplayWords); 
-  
+    if (relePin == HIGH){
+      needWater = 0;
+      StopPump();
+    } else{
+      Serial.print(DisplayWords); 
+    }
 
   } else {
     Serial.print(sensorValue);
   }
   delay(1000);
+}
+
+
+//Funksjon for å starte pumpa
+void StartPump(){
+  digitalWrite(relePin, HIGH);
+}
+
+//Funksjon for å stoppe punpa
+void StopPump(){
+  digitalWrite(relePin, LOW);
 }
