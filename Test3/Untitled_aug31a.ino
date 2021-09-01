877c64c2d3e7d7e70f5e5f1ff148530ed39b9242
bardpedersen <72338855+bardpedersen@users.noreply.github.com>
2021-09-01 12:59:39 +0200

    Update Untitled_aug31a.ino

--- Test3/Untitled_aug31a.ino
+++ Test3/Untitled_aug31a.ino
@@ -1,28 +1,28 @@
 #include "arduino_secrets.h"
-#include "thingProperties.h"
+#include "thingProperties.h" //Inkluderer filer fra Arduino Cloud 
 #include <DHT.h> 
-#define DHTPIN 3
+#define DHTPIN 3    //Definerer og inkulderer DHT sensor og bibliotek
 #define DHTTYPE 11
-#define MG_PIN                (0)     
+#define MG_PIN                (0)     //Definerer forskjellige pins for CO2 sensoren
 #define DC_GAIN               (8.5) 
 #define READ_SAMPLE_INTERVAL  (50) 
 #define READ_SAMPLE_TIMES     (50) 
-#define ZERO_POINT_VOLTAGE    (3.940) 
-#define REACTION_VOLTGAE      (1.940)
+#define ZERO_POINT_VOLTAGE    (3.940) //Setter referanse verdier til CO2 sensoren
+#define REACTION_VOLTGAE      (1.940) 
 
 DHT dht(DHTPIN,DHTTYPE);
-int photoPin = A2; //lys
-int airvalue = 1023; //Tørt
-int watervalue = 433; //Vått
-int soilmoisttpin = A1;
-int relePin = 2;
-float CO2Curve[3] = {2.602,ZERO_POINT_VOLTAGE,(REACTION_VOLTGAE/(2.602-3))};
+int photoPin = A2;      //lyssensor pin
+int airvalue = 1023;    //Jordfuktsmåleren sine verider i luft
+int watervalue = 433;   //Jordfuktsmåleren sine verider i vann
+int soilmoisttpin = A1; //Jordfuktsmåleren pin
+int relePin = 2;        //Rele pin
+float CO2Curve[3] = {2.602,ZERO_POINT_VOLTAGE,(REACTION_VOLTGAE/(2.602-3))}; //
 
 void setup() {
-  pinMode(photoPin, OUTPUT);
+  pinMode(photoPin, OUTPUT);  //Definerer pinen som en output verdi
   pinMode(relePin, OUTPUT);
-  Serial.begin(9600);
-  dht.begin();
+  Serial.begin(9600);         //Lager en kontakt mellom pc og Arduino MKR
+  dht.begin();                //Starter DHT sensorens fobindelse
   delay(1500); 
   initProperties();
   ArduinoCloud.begin(ArduinoIoTPreferredConnection);
@@ -30,45 +30,39 @@ void setup() {
   ArduinoCloud.printDebugInfo();
 }
 
-int needWater = 0;
-int waitTime = 5;
+int needWater = 0;  //
+int waitTime = 5;   //
 
 void loop() {
   ArduinoCloud.update();
-  float Temp = dht.readTemperature();
+  float Temp = dht.readTemperature();   //Definerer de forskjellige variablene
   float Hum = dht.readHumidity();
   int readvalue = analogRead(photoPin);
   int sensorValue = analogRead(soilmoisttpin);
   float volts = MGRead(MG_PIN);
   int percentage = MGGetPercentage(volts,CO2Curve);
   jordfukt = map(sensorValue, airvalue, watervalue, 0, 100);
-  temperatur = Temp;
+  temperatur = Temp;  //Overfører de forskjellige variablene til Arduino Cloud variabler
   LuftFukt = Hum;
   lys = readvalue;
   CO2 = percentage;
-  Serial.println(lys);
+  Serial.println(lys);  //Printer de forksjellige verdiene for å kontrollere at data blir overført til Arduino cloud 
   Serial.println(LuftFukt);
   Serial.println(temperatur);
   Serial.println(jordfukt);
   Serial.println(CO2);
   
-  if (50 > jordfukt > 0){
+  if (40 > jordfukt > 0){  //Løkke for å vanne planten hvis den måler under 40% jordfuktighet
     needWater = needWater + 1;
     if (needWater >= waitTime){
-      digitalWrite(relePin, HIGH);
-      delay(3000);
-      digitalWrite(relePin, LOW);
+      digitalWrite(relePin, HIGH); //Skrur på vannpumpa
+      delay(4000);                 //Tiden pumpen er på
+      digitalWrite(relePin, LOW);  //Skrur av vannpumpa
       needWater = 0;
     }
-  } 
-  else if (jordfukt < 50){
-    if (relePin == HIGH){
-      needWater = 0;
-      digitalWrite(relePin, LOW);
-    } 
-  } 
+  }  
   else {
-    needWater = 0;
+    needWater = 0; 
   }
   delay(1000);
 }
